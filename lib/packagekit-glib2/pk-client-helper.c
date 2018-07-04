5c559271faf496290b9d206150b8793cb73e0e37
Julian Andres Klode <julian.klode@canonical.com>
2018-07-04 12:16:23 +0200

    packagekit-glib2: Add pk_client_helper_is_active()
    This function enables code to check whether a client helper has
    accepted connections.
    A connection is considered active if one of its sources has not
    been removed yet, even if it's just blocking waiting for data.

--- lib/packagekit-glib2/pk-client-helper.c
+++ lib/packagekit-glib2/pk-client-helper.c
@@ -495,6 +495,28 @@ pk_client_helper_start (PkClientHelper *client_helper,
 	return TRUE;
 }
 
+/**
+ * pk_client_helper_is_active:
+ * @client_helper: a valid #PkClientHelper instance
+ *
+ * Return value: TRUE if there is an accepted connection, FALSE
+ *               otherwise.
+ *
+ * Since: 1.1.11
+ */
+gboolean
+pk_client_helper_is_active(PkClientHelper *client_helper)
+{
+	PkClientHelperPrivate *priv = client_helper->priv;
+	for (guint i = 0; i < priv->children->len; i++) {
+		PkClientHelperChild *child = g_ptr_array_index (priv->children, i);
+		if (!g_source_is_destroyed(child->socket_channel_source) && !g_source_is_destroyed(child->stdout_channel_source))
+			return TRUE;
+	}
+
+	return FALSE;
+}
+
 /*
  * pk_client_helper_class_init:
  **/
